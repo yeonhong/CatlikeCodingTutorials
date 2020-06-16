ca6c99dfbeac59dacbfc941f2e2d59c93bdd3ea8
yeonhong park <yeonhong84@gmail.com>
2020-06-16 14:00:36 +0900

    refactor basic 2 tutorial

--- Assets/Basics/2. building-a-graph/Graph.cs
+++ Assets/Basics/2. building-a-graph/Graph.cs
@@ -2,32 +2,32 @@
 
 public class Graph : MonoBehaviour
 {
-	public Transform pointPrefab;
-	[Range(10,100)]
-	public int resolution = 10;
+	[SerializeField] private Transform _pointPrefab;
+	[SerializeField] [Range(10,100)] private int _resolution = 10;
 
-	Transform[] points;
+	private Transform[] _points;
 
 	void Awake() {
-		float step = 2f / resolution;
-		Vector3 scale = Vector3.one * step;
-		Vector3 position = Vector3.zero;
+		float step = 2f / _resolution;
+		_points = new Transform[_resolution];
+		var scale = Vector3.one * step;
+		var position = Vector3.zero;
+		for (int i = 0; i < _points.Length; i++) {
+			Transform point = Instantiate(_pointPrefab);
 
-		points = new Transform[resolution];
-
-		for (int i = 0; i < points.Length; i++) {
-			Transform point = Instantiate(pointPrefab);
 			position.x = (i + 0.5f) * step - 1f;
+
 			point.localPosition = position;
 			point.localScale = scale;
+
 			point.SetParent(transform, false);
-			points[i] = point;
+			_points[i] = point;
 		}
 	}
 
 	private void Update() {
-		for (int i = 0; i < points.Length; i++) {
-			var point = points[i];
+		for (int i = 0; i < _points.Length; i++) {
+			var point = _points[i];
 			var position = point.localPosition;
 
 			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
