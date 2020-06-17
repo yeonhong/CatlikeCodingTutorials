d5268a73dbb7a72d8ce01b522c80740e19b96eb4
yeonhong park <yeonhong84@gmail.com>
2020-06-17 17:00:26 +0900

    5-1 Building an Atomic Nucleus

---
+++ Assets/Basics/5. Frames Per Second Measuring Performance/Nucleon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+	public float attractionForce = 10f;
+	private Rigidbody body;
+
+	private void Awake() {
+		body = GetComponent<Rigidbody>();
+	}
+
+	private void FixedUpdate() {
+		body.AddForce(transform.localPosition * -attractionForce);
+	}
+}
---
+++ Assets/Basics/5. Frames Per Second Measuring Performance/NucleonSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NucleonSpawner : MonoBehaviour
+{
+	public float timeBetweenSpawns = 0.05f;
+	public float spawnDistance = 15f;
+	public Nucleon[] nucleonPrefabs;
+
+	private float timeSinceLastSpawn;
+
+	private void FixedUpdate() {
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			timeSinceLastSpawn -= timeBetweenSpawns;
+			SpawnNucleon();
+		}
+	}
+
+	void SpawnNucleon() {
+		var prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+		var spawn = Instantiate(prefab);
+		spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+	}
+}
