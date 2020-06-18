9506cd7177f8b01c6e71c994b3b1f1c100ef4f69
yeonhong park <yeonhong84@gmail.com>
2020-06-18 17:35:58 +0900

    Rigidbody - Rigidbody Component - Controlling Rigidbody Velocity - Frictionless Movement - Fixed Update

--- Assets/2. Movement/1. Sliding a Sphere/MovingSphere.cs
+++ Assets/2. Movement/1. Sliding a Sphere/MovingSphere.cs
@@ -1,47 +1,50 @@
 ﻿using UnityEngine;
 
-public class MovingSphere : MonoBehaviour
+namespace Sliding_a_Sphere
 {
-	[SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
-	[SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f;
-	[SerializeField, Range(0f, 1f)] float bounciness = 0.5f;
-	private Vector3 velocity = Vector3.zero;
+	public class MovingSphere : MonoBehaviour
+	{
+		[SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
+		[SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f;
+		[SerializeField, Range(0f, 1f)] float bounciness = 0.5f;
+		private Vector3 velocity = Vector3.zero;
 
-	[SerializeField] Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
+		[SerializeField] Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
 
-	private void Update() {
-		Vector2 playerInput;
-		playerInput.x = Input.GetAxis("Horizontal");
-		playerInput.y = Input.GetAxis("Vertical");
-		playerInput.Normalize();
+		private void Update() {
+			Vector2 playerInput;
+			playerInput.x = Input.GetAxis("Horizontal");
+			playerInput.y = Input.GetAxis("Vertical");
+			playerInput.Normalize();
 
-		Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-		float maxSpeedChange = maxAcceleration * Time.deltaTime;
+			Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+			float maxSpeedChange = maxAcceleration * Time.deltaTime;
 
-		velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-		velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+			velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+			velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
 
-		Vector3 displacement = velocity * Time.deltaTime;
+			Vector3 displacement = velocity * Time.deltaTime;
 
-		Vector3 newPosition = transform.localPosition + displacement;
+			Vector3 newPosition = transform.localPosition + displacement;
 
-		if (newPosition.x < allowedArea.xMin) {
-			newPosition.x = allowedArea.xMin;
-			velocity.x = -velocity.x * bounciness;
+			if (newPosition.x < allowedArea.xMin) {
+				newPosition.x = allowedArea.xMin;
+				velocity.x = -velocity.x * bounciness;
+			}
+			else if (newPosition.x > allowedArea.xMax) {
+				newPosition.x = allowedArea.xMax;
+				velocity.x = -velocity.x * bounciness;
+			}
+			if (newPosition.z < allowedArea.yMin) {
+				newPosition.z = allowedArea.yMin;
+				velocity.z = -velocity.z * bounciness;
+			}
+			else if (newPosition.z > allowedArea.yMax) {
+				newPosition.z = allowedArea.yMax;
+				velocity.z = -velocity.z * bounciness;
+			}
+
+			transform.localPosition = newPosition;
 		}
-		else if (newPosition.x > allowedArea.xMax) {
-			newPosition.x = allowedArea.xMax;
-			velocity.x = -velocity.x * bounciness;
-		}
-		if (newPosition.z < allowedArea.yMin) {
-			newPosition.z = allowedArea.yMin;
-			velocity.z = -velocity.z * bounciness;
-		}
-		else if (newPosition.z > allowedArea.yMax) {
-			newPosition.z = allowedArea.yMax;
-			velocity.z = -velocity.z * bounciness;
-		}
-
-		transform.localPosition = newPosition;
-	}
+	} 
 }
---
+++ Assets/2. Movement/2. Physics/MovingSphere.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace PhysicsTutorial
+{
+	public class MovingSphere : MonoBehaviour
+	{
+		[SerializeField, Range(0f, 100f)] private float maxSpeed = 10f;
+		[SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f;
+		private Rigidbody body;
+		Vector3 desiredVelocity;
+
+		private void Awake() {
+			body = GetComponent<Rigidbody>();
+		}
+
+		private void Update() {
+			Vector2 playerInput;
+			playerInput.x = Input.GetAxis("Horizontal");
+			playerInput.y = Input.GetAxis("Vertical");
+			playerInput.Normalize();
+
+			desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+		}
+
+		private void FixedUpdate() {
+			float maxSpeedChange = maxAcceleration * Time.deltaTime;
+
+			var velocity = body.velocity;
+			velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+			velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+
+			body.velocity = velocity;
+		}
+	}
+}
