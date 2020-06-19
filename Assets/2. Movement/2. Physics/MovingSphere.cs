165ed3dff0d4f8aadc4d3f3a9d6da3711473259f
yeonhong park <yeonhong84@gmail.com>
2020-06-19 15:17:18 +0900

    Slopes - Slope Testing - Ground Angle - Jumping While on a Slope - Moving Along Slopes - Multiple Ground Normals - Counting Ground Contacts

--- Assets/2. Movement/2. Physics/MovingSphere.cs
+++ Assets/2. Movement/2. Physics/MovingSphere.cs
@@ -10,14 +10,24 @@ namespace PhysicsTutorial
 		[SerializeField, Range(0f, 10f)] private float jumpHeight = 2f;
 		[SerializeField, Range(0, 5)] private int maxAirJumps = 1;
 
+		[SerializeField, Range(0f, 90f)] private float maxGroundAngle = 25f;
+
 		private Rigidbody body;
 		private Vector3 velocity, desiredVelocity;
 		private bool desiredJump;
-		private bool onGround;
+		private int groundContactCount;
+		private bool OnGround => groundContactCount > 0;
 		private int jumpPhase;
+		private float minGroundDotProduct;
+		private Vector3 contactNormal;
+
+		private void OnValidate() {
+			minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+		}
 
 		private void Awake() {
 			body = GetComponent<Rigidbody>();
+			OnValidate();
 		}
 
 		private void Update() {
@@ -45,17 +55,16 @@ namespace PhysicsTutorial
 			// 그러나 여유있게 0.9 이상의 Y 구성 요소를 수용합니다.
 			for (int i = 0; i < collision.contactCount; i++) {
 				Vector3 normal = collision.GetContact(i).normal;
-				onGround |= normal.y >= 0.9f;
+				if (normal.y >= minGroundDotProduct) {
+					groundContactCount += 1;
+					contactNormal += normal;
+				}
 			}
 		}
 
 		private void FixedUpdate() {
 			UpdateState();
-
-			float acceleration = onGround ? maxAcceleration : maxAirAcceleration;
-			float maxSpeedChange = acceleration * Time.deltaTime;
-			velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-			velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+			AdjustVelocity();
 
 			if (desiredJump) {
 				desiredJump = false;
@@ -64,25 +73,57 @@ namespace PhysicsTutorial
 
 			body.velocity = velocity;
 
-			onGround = false;
+			ClearState();
 		}
 
-		void UpdateState() {
+		private void ClearState() {
+			groundContactCount = 0;
+			contactNormal = Vector3.zero;
+		}
+
+		private void UpdateState() {
 			velocity = body.velocity;
-			if (onGround) {
+			if (OnGround) {
 				jumpPhase = 0;
+				if (groundContactCount > 1) {
+					contactNormal.Normalize();
+				}
+			}
+			else {
+				contactNormal = Vector3.up;
 			}
 		}
 
 		private void Jump() {
-			if (onGround || jumpPhase < maxAirJumps) {
+			if (OnGround || jumpPhase < maxAirJumps) {
 				jumpPhase++;
 				float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-				if (velocity.y > 0f) {
-					jumpSpeed = Mathf.Max(jumpSpeed - velocity.y, 0f);
+				float alignedSpeed = Vector3.Dot(velocity, contactNormal);
+				if (alignedSpeed > 0f) {
+					jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
 				}
-				velocity.y += jumpSpeed;
+				velocity += contactNormal * jumpSpeed;
 			}
+		}
+
+		private Vector3 ProjectOnContactPlane(Vector3 vector) {
+			return vector - contactNormal * Vector3.Dot(vector, contactNormal);
+		}
+
+		private void AdjustVelocity() {
+			Vector3 xAxis = ProjectOnContactPlane(Vector3.right).normalized;
+			Vector3 zAxis = ProjectOnContactPlane(Vector3.forward).normalized;
+
+			float currentX = Vector3.Dot(velocity, xAxis);
+			float currentZ = Vector3.Dot(velocity, zAxis);
+
+			float acceleration = OnGround ? maxAcceleration : maxAirAcceleration;
+			float maxSpeedChange = acceleration * Time.deltaTime;
+
+			float newX = Mathf.MoveTowards(currentX, desiredVelocity.x, maxSpeedChange);
+			float newZ = Mathf.MoveTowards(currentZ, desiredVelocity.z, maxSpeedChange);
+
+			velocity += xAxis * (newX - currentX) + zAxis * (newZ - currentZ);
 		}
 	}
 }
