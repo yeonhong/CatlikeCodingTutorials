640b891b58975b7423574c89399dd2f8346042b3
yeonhong park <yeonhong84@gmail.com>
2020-06-22 14:25:57 +0900

    Steep Contacts (가파른 접점, 틈에 빠지거나) - Detecting Steep Contacts - Crevasses - Wall Jumping - Air Jumping - Upward Jump Bias

--- Assets/2. Movement/3. Surface Contact/MovingSphere.cs
+++ Assets/2. Movement/3. Surface Contact/MovingSphere.cs
@@ -17,12 +17,14 @@ namespace SurfaceContract
 		private Rigidbody body;
 		private Vector3 velocity, desiredVelocity;
 		private bool desiredJump;
-		private int groundContactCount;
-		private bool OnGround => groundContactCount > 0;
 		private int jumpPhase;
 		private float minGroundDotProduct, minStairsDotProduct;
-		private Vector3 contactNormal;
+		private Vector3 contactNormal, steepNormal;
+		private int groundContactCount, steepContactCount;
 		private int stepsSinceLastGrounded, stepsSinceLastJump;
+
+		private bool OnGround => groundContactCount > 0;
+		private bool OnSteep => steepContactCount > 0;
 
 		private void OnValidate() {
 			minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
@@ -63,6 +65,10 @@ namespace SurfaceContract
 					groundContactCount += 1;
 					contactNormal += normal;
 				}
+				else if (normal.y > -0.01f) { // 가파른 곳인지
+					steepContactCount += 1;
+					steepNormal += normal;
+				}
 			}
 		}
 
@@ -81,8 +87,8 @@ namespace SurfaceContract
 		}
 
 		private void ClearState() {
-			groundContactCount = 0;
-			contactNormal = Vector3.zero;
+			groundContactCount = steepContactCount = 0;
+			contactNormal = steepNormal = Vector3.zero;
 		}
 
 		private void UpdateState() {
@@ -90,9 +96,11 @@ namespace SurfaceContract
 			stepsSinceLastJump++;
 
 			velocity = body.velocity;
-			if (OnGround || SnapToGround()) {
+			if (OnGround || SnapToGround() || CheckSteepContacts()) {
 				stepsSinceLastGrounded = 0;
-				jumpPhase = 0;
+				if (stepsSinceLastJump > 1) {
+					jumpPhase = 0;
+				}
 				if (groundContactCount > 1) {
 					contactNormal.Normalize();
 				}
@@ -133,16 +141,34 @@ namespace SurfaceContract
 		}
 
 		private void Jump() {
-			if (OnGround || jumpPhase < maxAirJumps) {
-				stepsSinceLastJump = 0;
-				jumpPhase++;
-				float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
-				float alignedSpeed = Vector3.Dot(velocity, contactNormal);
-				if (alignedSpeed > 0f) {
-					jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
-				}
-				velocity += contactNormal * jumpSpeed;
+			Vector3 jumpDirection = Vector3.zero;
+
+			if (OnGround) {
+				jumpDirection = contactNormal;
 			}
+			else if (OnSteep) {
+				jumpDirection = steepNormal;
+				jumpPhase = 0;
+			}
+			else if (maxAirJumps > 0 && jumpPhase <= maxAirJumps) {
+				if (jumpPhase == 0) {
+					jumpPhase = 1;
+				}
+				jumpDirection = contactNormal;
+			}
+			else {
+				return;
+			}
+
+			stepsSinceLastJump = 0;
+			jumpPhase++;
+			float jumpSpeed = Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+			jumpDirection = (jumpDirection + Vector3.up).normalized;
+			float alignedSpeed = Vector3.Dot(velocity, jumpDirection);
+			if (alignedSpeed > 0f) {
+				jumpSpeed = Mathf.Max(jumpSpeed - alignedSpeed, 0f);
+			}
+			velocity += jumpDirection * jumpSpeed;
 		}
 
 		private Vector3 ProjectOnContactPlane(Vector3 vector) {
@@ -168,6 +194,18 @@ namespace SurfaceContract
 		private float GetMinDot(int layer) {
 			return (stairsMask & (1 << layer)) == 0 ?
 				minGroundDotProduct : minStairsDotProduct;
+		}
+
+		private bool CheckSteepContacts() {
+			if (steepContactCount > 1) {
+				steepNormal.Normalize();
+				if (steepNormal.y >= minGroundDotProduct) {
+					groundContactCount = 1;
+					contactNormal = steepNormal;
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 }
