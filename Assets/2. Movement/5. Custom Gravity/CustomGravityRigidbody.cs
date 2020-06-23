f85836314cefecbe0fa9cfa679c6b11f37f56a85
yeonhong park <yeonhong84@gmail.com>
2020-06-23 17:33:37 +0900

    Other Bodies - Specialized Rigidbody Component - Sleeping - Staying Awake

---
+++ Assets/2. Movement/5. Custom Gravity/CustomGravityRigidbody.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CustomGravityRigidbody : MonoBehaviour
+{
+	private Rigidbody body;
+	private float floatDelay;
+	[SerializeField] bool floatToSleep = true;
+
+	private void Awake() {
+		body = GetComponent<Rigidbody>();
+		body.useGravity = false;
+	}
+
+	private void FixedUpdate() {
+		if (floatToSleep) {
+			if (body.IsSleeping()) {
+				floatDelay = 0f;
+				return;
+			}
+
+			if (body.velocity.sqrMagnitude < 0.0001f) {
+				floatDelay += Time.deltaTime;
+				if (floatDelay >= 1f) {
+					return;
+				}
+			}
+			else {
+				floatDelay = 0f;
+			}
+		}
+
+		body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
+	}
+}
