80ef7f7c8a4558f680326144c702a085351f7283
yeonhong park <yeonhong84@gmail.com>
2020-06-23 17:57:45 +0900

    Complex Gravity 준비

--- Assets/2. Movement/5. Custom Gravity/CustomGravityRigidbody.cs
+++ Assets/2. Movement/5. Custom Gravity/CustomGravityRigidbody.cs
@@ -1,35 +1,38 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class CustomGravityRigidbody : MonoBehaviour
+namespace CustomGravityTutorial
 {
-	private Rigidbody body;
-	private float floatDelay;
-	[SerializeField] bool floatToSleep = true;
+	[RequireComponent(typeof(Rigidbody))]
+	public class CustomGravityRigidbody : MonoBehaviour
+	{
+		private Rigidbody body;
+		private float floatDelay;
+		[SerializeField] bool floatToSleep = true;
 
-	private void Awake() {
-		body = GetComponent<Rigidbody>();
-		body.useGravity = false;
-	}
-
-	private void FixedUpdate() {
-		if (floatToSleep) {
-			if (body.IsSleeping()) {
-				floatDelay = 0f;
-				return;
-			}
-
-			if (body.velocity.sqrMagnitude < 0.0001f) {
-				floatDelay += Time.deltaTime;
-				if (floatDelay >= 1f) {
-					return;
-				}
-			}
-			else {
-				floatDelay = 0f;
-			}
+		private void Awake() {
+			body = GetComponent<Rigidbody>();
+			body.useGravity = false;
 		}
 
-		body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
+		private void FixedUpdate() {
+			if (floatToSleep) {
+				if (body.IsSleeping()) {
+					floatDelay = 0f;
+					return;
+				}
+
+				if (body.velocity.sqrMagnitude < 0.0001f) {
+					floatDelay += Time.deltaTime;
+					if (floatDelay >= 1f) {
+						return;
+					}
+				}
+				else {
+					floatDelay = 0f;
+				}
+			}
+
+			body.AddForce(CustomGravity.GetGravity(body.position), ForceMode.Acceleration);
+		}
 	}
 }
