6376542de4cd24edca8baced3569518a9bd20879
yeonhong park <yeonhong84@gmail.com>
2020-06-24 11:20:08 +0900

    Multiple Gravity Sources - Default Gravity Source - Gravity Source List - Registering and Unregistering Sources - Allowing Extension

---
+++ Assets/2. Movement/6. Complex Gravity/GravitySource.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ComplexGravityTutorial
+{
+	public class GravitySource : MonoBehaviour
+	{
+		public virtual Vector3 GetGravity(Vector3 position) {
+			return Physics.gravity;
+		}
+
+		void OnEnable() {
+			CustomGravity.Register(this);
+		}
+
+		void OnDisable() {
+			CustomGravity.Unregister(this);
+		}
+	} 
+}
