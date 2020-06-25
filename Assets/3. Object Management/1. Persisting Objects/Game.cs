3044d28da2fcfe1dc7bb1cad4b0cbc09ae7c2c01
yeonhong park <yeonhong84@gmail.com>
2020-06-25 16:07:13 +0900

    Creating Objects On Demand - Game Logic - Player Input - Randomized Cubes - Starting a New Game - Keeping Track of Objects - Clearing the List

---
+++ Assets/3. Object Management/1. Persisting Objects/Game.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectManagement
+{
+	public class Game : MonoBehaviour
+	{
+		public Transform prefab = null;
+		public KeyCode createKey = KeyCode.C;
+		public KeyCode newGameKey = KeyCode.N;
+
+		List<Transform> objects = null;
+
+		void Awake() {
+			objects = new List<Transform>();
+		}
+
+		private void Update() {
+			if (Input.GetKeyDown(createKey)) {
+				CreateObject();
+			}
+			else if (Input.GetKey(newGameKey)) {
+				BeginNewGame();
+			}
+		}
+
+		private void BeginNewGame() {
+			for (int i = 0; i < objects.Count; i++) {
+				Destroy(objects[i].gameObject);
+			}
+			objects.Clear();
+		}
+
+		private void CreateObject() {
+			Transform t = Instantiate(prefab);
+			t.localPosition = Random.insideUnitSphere * 5f;
+			t.localRotation = Random.rotation;
+			t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+			objects.Add(t);
+		}
+	}
+}
