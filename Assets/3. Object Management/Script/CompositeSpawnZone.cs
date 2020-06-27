38954f09e2f7eef693c36c6d21c5ca970b1b008b
yeonhong park <yeonhong84@gmail.com>
2020-06-27 10:57:42 +0900

    Zone Types - Absctract Spawn Zone - Sphere Zone - Cube Zone - Composite Zone

---
+++ Assets/3. Object Management/Script/CompositeSpawnZone.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace ObjectManagement
+{
+	public class CompositeSpawnZone : SpawnZone
+	{
+		[SerializeField] private SpawnZone[] spawnZones = null;
+
+		public override Vector3 SpawnPoint {
+			get {
+				int index = Random.Range(0, spawnZones.Length);
+				return spawnZones[index].SpawnPoint;
+			}
+		}
+	}
+}
--- Assets/3. Object Management/Script/SpawnZone.cs
+++ Assets/3. Object Management/Script/SpawnZone.cs
@@ -2,22 +2,8 @@
 
 namespace ObjectManagement
 {
-	public class SpawnZone : MonoBehaviour
+	public abstract class SpawnZone : MonoBehaviour
 	{
-		[SerializeField] private bool surfaceOnly = true;
-
-		public Vector3 SpawnPoint {
-			get {
-				return transform.TransformPoint(
-					surfaceOnly ? Random.onUnitSphere : Random.insideUnitSphere
-				);
-			}
-		}
-
-		void OnDrawGizmos() {
-			Gizmos.color = Color.cyan;
-			Gizmos.matrix = transform.localToWorldMatrix;
-			Gizmos.DrawWireSphere(Vector3.zero, 1f);
-		}
+		public abstract Vector3 SpawnPoint { get; }
 	}
 }
