a430b457394adb8536ffe235fd1839028d060994
yeonhong park <yeonhong84@gmail.com>
2020-07-09 22:54:18 +0900

    Cyclic Scenarios - Cycling through Waves - Speeding Up

--- Assets/4. Tower Defense/Scripts/GameScenario.cs
+++ Assets/4. Tower Defense/Scripts/GameScenario.cs
@@ -6,6 +6,8 @@ namespace TowerDefense
 	public class GameScenario : ScriptableObject
 	{
 		[SerializeField] private EnemyWave[] waves = { };
+		[SerializeField, Range(0, 10)] private int cycles = 1;
+		[SerializeField, Range(0f, 1f)] private float cycleSpeedUp = 0.5f;
 
 		public State Begin() => new State(this);
 
@@ -13,21 +15,28 @@ namespace TowerDefense
 		public struct State
 		{
 			private GameScenario scenario;
-			private int index;
+			private int cycle, index;
 			private EnemyWave.State wave;
+			private float timeScale;
 
 			public State(GameScenario scenario) {
 				this.scenario = scenario;
+				cycle = 0;
 				index = 0;
 				Debug.Assert(scenario.waves.Length > 0, "Empty scenario!");
 				wave = scenario.waves[0].Begin();
+				timeScale = 1f;
 			}
 
 			public bool Progress() {
-				float deltaTime = wave.Progress(Time.deltaTime);
+				float deltaTime = wave.Progress(timeScale * Time.deltaTime);
 				while (deltaTime >= 0f) {
 					if (++index >= scenario.waves.Length) {
-						return false;
+						if (++cycle >= scenario.cycles && scenario.cycles > 0) {
+							return false;
+						}
+						index = 0;
+						timeScale += scenario.cycleSpeedUp;
 					}
 					wave = scenario.waves[index].Begin();
 					deltaTime = wave.Progress(deltaTime);
