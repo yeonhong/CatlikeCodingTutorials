26173c57becead47ca272d63cf9a3709d4451e91
yeonhong park <yeonhong84@gmail.com>
2020-07-12 00:04:41 +0900

    4. Generating Additonal Vertex Data

--- Assets/5. Mesh Basics/Scripts/Grid.cs
+++ Assets/5. Mesh Basics/Scripts/Grid.cs
@@ -19,12 +19,19 @@ namespace MeshBasics
 			mesh.name = "Procedural Grid";
 
 			vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+			Vector2[] uv = new Vector2[vertices.Length];
+			Vector4[] tangents = new Vector4[vertices.Length];
+			Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
 			for (int i = 0, y = 0; y <= ySize; y++) {
 				for (int x = 0; x <= xSize; x++, i++) {
 					vertices[i] = new Vector3(x, y);
+					uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+					tangents[i] = tangent;
 				}
 			}
 			mesh.vertices = vertices;
+			mesh.uv = uv;
+			mesh.tangents = tangents;
 
 			int[] triangles = new int[xSize * ySize * 6];
 			for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
@@ -36,6 +43,7 @@ namespace MeshBasics
 				}
 			}
 			mesh.triangles = triangles;
+			mesh.RecalculateNormals();
 		}
 
 		private void OnDrawGizmos() {
