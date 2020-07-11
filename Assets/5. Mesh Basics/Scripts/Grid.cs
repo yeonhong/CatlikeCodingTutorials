bbe60564ba185c77a19a10de986f03eb1574e75b
yeonhong park <yeonhong84@gmail.com>
2020-07-11 23:52:36 +0900

    3. Creating the Mesh

--- Assets/5. Mesh Basics/Scripts/Grid.cs
+++ Assets/5. Mesh Basics/Scripts/Grid.cs
@@ -8,20 +8,34 @@ namespace MeshBasics
 	{
 		public int xSize, ySize;
 		private Vector3[] vertices;
+		private Mesh mesh;
 
 		private void Awake() {
-			StartCoroutine(Generate());
+			Generate();
 		}
 
-		private IEnumerator Generate() {
-			WaitForSeconds wait = new WaitForSeconds(0.05f);
+		private void Generate() {
+			GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+			mesh.name = "Procedural Grid";
+
 			vertices = new Vector3[(xSize + 1) * (ySize + 1)];
 			for (int i = 0, y = 0; y <= ySize; y++) {
 				for (int x = 0; x <= xSize; x++, i++) {
 					vertices[i] = new Vector3(x, y);
-					yield return wait;
 				}
 			}
+			mesh.vertices = vertices;
+
+			int[] triangles = new int[xSize * ySize * 6];
+			for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++) {
+				for (int x = 0; x < xSize; x++, ti += 6, vi++) {
+					triangles[ti] = vi;
+					triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+					triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+					triangles[ti + 5] = vi + xSize + 2;
+				}
+			}
+			mesh.triangles = triangles;
 		}
 
 		private void OnDrawGizmos() {
