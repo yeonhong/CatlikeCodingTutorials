b6c6bb61d630fda7394fe7c89c7028fee529bed3
yeonhong park <yeonhong84@gmail.com>
2020-07-13 15:56:00 +0900

    [Mesh Deformation] 1. Scene Setup 2. Mesh Deformer - Preparation - Vertex Velocities

---
+++ Assets/5. Mesh Basics/Scripts/MeshDeformer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace MeshBasics
+{
+	[RequireComponent(typeof(MeshFilter))]
+	public class MeshDeformer : MonoBehaviour
+	{
+		private Mesh deformingMesh;
+		private Vector3[] originalVertices, displacedVertices;
+		private Vector3[] vertexVelocities;
+
+		private void Start() {
+			deformingMesh = GetComponent<MeshFilter>().mesh;
+			originalVertices = deformingMesh.vertices;
+			displacedVertices = new Vector3[originalVertices.Length];
+			for (int i = 0; i < originalVertices.Length; i++) {
+				displacedVertices[i] = originalVertices[i];
+			}
+
+			vertexVelocities = new Vector3[originalVertices.Length];
+		}
+	}
+}
