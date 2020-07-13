3481b7d63926f849ae2d31562fbda28866c79448
yeonhong park <yeonhong84@gmail.com>
2020-07-13 17:35:10 +0900

    5. Staying in Shape - Springs - Damping 6. Dealing with Transformations - Adjusting Scale

--- Assets/5. Mesh Basics/Scripts/MeshDeformer.cs
+++ Assets/5. Mesh Basics/Scripts/MeshDeformer.cs
@@ -5,9 +5,13 @@ namespace MeshBasics
 	[RequireComponent(typeof(MeshFilter))]
 	public class MeshDeformer : MonoBehaviour
 	{
+		public float springForce = 20f;
+		public float damping = 5f;
+
 		private Mesh deformingMesh;
 		private Vector3[] originalVertices, displacedVertices;
 		private Vector3[] vertexVelocities;
+		private float uniformScale = 1f;
 
 		private void Start() {
 			deformingMesh = GetComponent<MeshFilter>().mesh;
@@ -22,6 +26,7 @@ namespace MeshBasics
 
 		public void AddDeformingForce(Vector3 point, float force) {
 			Debug.DrawLine(Camera.main.transform.position, point);
+			point = transform.InverseTransformPoint(point);
 			for (int i = 0; i < displacedVertices.Length; i++) {
 				AddForceToVertex(i, point, force);
 			}
@@ -29,12 +34,14 @@ namespace MeshBasics
 
 		private void AddForceToVertex(int i, Vector3 point, float force) {
 			Vector3 pointToVertex = displacedVertices[i] - point;
+			pointToVertex *= uniformScale;
 			float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
 			float velocity = attenuatedForce * Time.deltaTime;
 			vertexVelocities[i] += pointToVertex.normalized * velocity;
 		}
 
 		private void Update() {
+			uniformScale = transform.localScale.x;
 			for (int i = 0; i < displacedVertices.Length; i++) {
 				UpdateVertex(i);
 			}
@@ -44,7 +51,12 @@ namespace MeshBasics
 
 		private void UpdateVertex(int i) {
 			Vector3 velocity = vertexVelocities[i];
-			displacedVertices[i] += velocity * Time.deltaTime;
+			Vector3 displacement = displacedVertices[i] - originalVertices[i];
+			displacement *= uniformScale;
+			velocity -= displacement * springForce * Time.deltaTime;
+			velocity *= 1f - damping * Time.deltaTime;
+			vertexVelocities[i] = velocity;
+			displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
 		}
 	}
 }
