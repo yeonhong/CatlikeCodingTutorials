03ee75016495ccac5dcc36abe2eed440f12d0e79
yeonhong park <yeonhong84@gmail.com>
2020-07-13 15:44:56 +0900

    3. Playing with Math 4. Adjusting the Mapping

--- Assets/5. Mesh Basics/Scripts/CircleGizmo.cs
+++ Assets/5. Mesh Basics/Scripts/CircleGizmo.cs
@@ -5,22 +5,28 @@ namespace MeshBasics
 	public class CircleGizmo : MonoBehaviour
 	{
 		public int resolution = 10;
+		public bool bAdvance = false;
 
 		private void OnDrawGizmosSelected() {
 			float step = 2f / resolution;
 			for (int i = 0; i <= resolution; i++) {
-				ShowPoint(i * step - 1f, -1f);
-				ShowPoint(i * step - 1f, 1f);
+				ShowPoint(i * step - 1f, -1f, bAdvance);
+				ShowPoint(i * step - 1f, 1f, bAdvance);
 			}
 			for (int i = 1; i < resolution; i++) {
-				ShowPoint(-1f, i * step - 1f);
-				ShowPoint(1f, i * step - 1f);
+				ShowPoint(-1f, i * step - 1f, bAdvance);
+				ShowPoint(1f, i * step - 1f, bAdvance);
 			}
 		}
 
-		private void ShowPoint(float x, float y) {
+		private void ShowPoint(float x, float y, bool advance = false) {
 			Vector2 square = new Vector2(x, y);
 			Vector2 circle = square.normalized;
+
+			if (advance) {
+				circle.x = square.x * Mathf.Sqrt(1f - square.y * square.y * 0.5f);
+				circle.y = square.y * Mathf.Sqrt(1f - square.x * square.x * 0.5f);
+			}
 
 			Gizmos.color = Color.black;
 			Gizmos.DrawSphere(square, 0.025f);
--- Assets/5. Mesh Basics/Scripts/CubeSphere.cs
+++ Assets/5. Mesh Basics/Scripts/CubeSphere.cs
@@ -8,6 +8,7 @@ namespace MeshBasics
 	{
 		public int gridSize;
 		public float radius = 1f;
+		public bool isAdvance = false;
 
 		private Mesh mesh;
 		private Vector3[] vertices;
@@ -83,8 +84,21 @@ namespace MeshBasics
 		}
 
 		private void SetVertex(int i, int x, int y, int z) {
-			Vector3 v = new Vector3(x, y, z) * 2f / gridSize - Vector3.one;
-			normals[i] = v.normalized;
+			if (isAdvance) {
+				Vector3 v = new Vector3(x, y, z) * 2f / gridSize - Vector3.one;
+				float x2 = v.x * v.x;
+				float y2 = v.y * v.y;
+				float z2 = v.z * v.z;
+				Vector3 s;
+				s.x = v.x * Mathf.Sqrt(1f - y2 / 2f - z2 / 2f + y2 * z2 / 3f);
+				s.y = v.y * Mathf.Sqrt(1f - x2 / 2f - z2 / 2f + x2 * z2 / 3f);
+				s.z = v.z * Mathf.Sqrt(1f - x2 / 2f - y2 / 2f + x2 * y2 / 3f);
+				normals[i] = s;
+			} else {
+				Vector3 v = new Vector3(x, y, z) * 2f / gridSize - Vector3.one;
+				normals[i] = v.normalized;
+			}
+
 			vertices[i] = normals[i] * radius;
 			cubeUV[i] = new Color32((byte)x, (byte)y, (byte)z, 0);
 		}
