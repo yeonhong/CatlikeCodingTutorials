2c73f29e8f73b4e969ecf6710ad9738b6c614d2b
yeonhong park <yeonhong84@gmail.com>
2020-07-14 15:39:41 +0900

    [Rendering1] -  Matrices 5. Projection Matrices - Orthographic Camera - Perspective Camera

---
+++ Assets/6. Rendering/Scripts/CameraTransformation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Rendering
+{
+	public class CameraTransformation : Transformation
+	{
+		public bool isOrtho = false;
+		public float focalLength = 1f;
+
+		public override Matrix4x4 Matrix {
+
+			get {
+				Matrix4x4 matrix = new Matrix4x4();
+				if (isOrtho) {
+					matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+					matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+					matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+					matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+				} else {
+					matrix.SetRow(0, new Vector4(focalLength, 0f, 0f, 0f));
+					matrix.SetRow(1, new Vector4(0f, focalLength, 0f, 0f));
+					matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+					matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
+				}
+				return matrix;
+			}
+		}
+	}
+}
