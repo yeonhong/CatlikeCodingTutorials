93b22503f2389aa4bc0bd5d633a7442a84a8aa5f
yeonhong park <yeonhong84@gmail.com>
2020-07-28 12:48:04 +0900

    [Custom Render Pipeline] 1. A new Render Pipeline - Project Setup - Pipeline Asset - Render Pipeline Instance

---
+++ Assets/7. Scriptable Render Pipeline/Custom RP/Runtime/CustomRenderPipeline.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace CustomRP
+{
+	public class CustomRenderPipeline : RenderPipeline
+	{
+		protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
+		}
+	}
+}
