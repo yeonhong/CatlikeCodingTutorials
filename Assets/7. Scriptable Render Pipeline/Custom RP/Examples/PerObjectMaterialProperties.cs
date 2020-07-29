93ad11bddbd47443863efbfa2cab9184d486a4c6
yeonhong park <yeonhong84@gmail.com>
2020-07-29 13:32:07 +0900

    [Draw Calls] - Shaders and Batches 3. Transparency - Blend Modes - Not Writing Depth - Texturing - Alpha Clipping - Shader Features - Cutoff Per Object - Ball of Alpha-Clipped Spheres

--- Assets/7. Scriptable Render Pipeline/Custom RP/Examples/PerObjectMaterialProperties.cs
+++ Assets/7. Scriptable Render Pipeline/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -6,20 +6,25 @@ namespace CustomRP
 	public class PerObjectMaterialProperties : MonoBehaviour
 	{
 		private static int baseColorId = Shader.PropertyToID("_BaseColor");
+		private static int cutoffId = Shader.PropertyToID("_Cutoff");
 		private static MaterialPropertyBlock block;
 
 		[SerializeField]
 		private Color baseColor = Color.white;
 
-		void Awake() {
+		[SerializeField, Range(0f, 1f)]
+		float cutoff = 0.5f;
+
+		private void Awake() {
 			OnValidate();
 		}
 
-		void OnValidate() {
+		private void OnValidate() {
 			if (block == null) {
 				block = new MaterialPropertyBlock();
 			}
 			block.SetColor(baseColorId, baseColor);
+			block.SetFloat(cutoffId, cutoff);
 			GetComponent<Renderer>().SetPropertyBlock(block);
 		}
 	}
