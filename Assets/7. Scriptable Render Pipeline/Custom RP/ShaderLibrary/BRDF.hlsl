25a3826bc2c2834cb2df51492042d71b562c940a
yeonhong park <yeonhong84@gmail.com>
2020-07-29 17:49:00 +0900

    [Directional Lights] - Direct Illumination 4. Transparency - Premultiplied Alpha - Premultiplication Toggle

--- Assets/7. Scriptable Render Pipeline/Custom RP/ShaderLibrary/BRDF.hlsl
+++ Assets/7. Scriptable Render Pipeline/Custom RP/ShaderLibrary/BRDF.hlsl
@@ -14,10 +14,13 @@ float OneMinusReflectivity(float metallic) {
 	return range - metallic * range;
 }
 
-BRDF GetBRDF(Surface surface) {
+BRDF GetBRDF(Surface surface, bool applyAlphaToDiffuse = false) {
 	BRDF brdf;
 	float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
 	brdf.diffuse = surface.color * oneMinusReflectivity;
+	if (applyAlphaToDiffuse) {
+		brdf.diffuse *= surface.alpha;
+	}
 	brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
 	float perceptualRoughness = PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
 	brdf.roughness = PerceptualRoughnessToRoughness(perceptualRoughness);
