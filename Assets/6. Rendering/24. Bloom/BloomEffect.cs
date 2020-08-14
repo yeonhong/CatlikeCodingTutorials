053a86a7a2d4cd6296df121e9f3f448dd91b0909
yeonhong park <yeonhong84@gmail.com>
2020-08-14 17:31:43 +0900

    [Bloom] Blurring Light 1. Setting the Scene - Bloom Scene - Bloom Effect

---
+++ Assets/6. Rendering/24. Bloom/BloomEffect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+namespace Rendering
+{
+	[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+	public class BloomEffect : MonoBehaviour
+	{
+		void OnRenderImage(RenderTexture source, RenderTexture destination) {
+			Graphics.Blit(source, destination);
+		}
+	} 
+}
