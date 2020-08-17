bfd6a30356b06762ce13f9765a4a17008ec83018
yeonhong park <yeonhong84@gmail.com>
2020-08-17 18:11:45 +0900

    [Depth of Field] - Bending Light 1. Setting the Scene - Setting the Scene

---
+++ Assets/6. Rendering/25. Depth of Field/DepthOfFieldEffect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Rendering
+{
+	[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+	public class DepthOfFieldEffect : MonoBehaviour
+	{
+		[HideInInspector]
+		public Shader dofShader;
+
+		[NonSerialized]
+		private Material dofMaterial;
+
+		private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+			if (dofMaterial == null) {
+				dofMaterial = new Material(dofShader);
+				dofMaterial.hideFlags = HideFlags.HideAndDontSave;
+			}
+
+			Graphics.Blit(source, destination, dofMaterial);
+		}
+	}
+}
