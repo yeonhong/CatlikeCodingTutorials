7b627ae5d07cbae7f377b9f211a46a84d6d2c2e9
yeonhong park <yeonhong84@gmail.com>
2020-09-24 06:22:17 +0900

    [Elevation] 2. Terraced Edge Connections - Triangulation

--- Assets/9. Hex Map/Scripts/HexMetrics.cs
+++ Assets/9. Hex Map/Scripts/HexMetrics.cs
@@ -11,6 +11,9 @@ namespace HexMap
 
 		public const float elevationStep = 5f;
 
+		public const int terracesPerSlope = 2;
+		public const int terraceSteps = terracesPerSlope * 2 + 1;
+
 		public static Vector3[] corners = {
 			new Vector3(0f, 0f, outerRadius),
 			new Vector3(innerRadius, 0f, 0.5f * outerRadius),
@@ -40,6 +43,23 @@ namespace HexMap
 		public static Vector3 GetBridge(HexDirection direction) {
 			return (corners[(int)direction] + corners[(int)direction + 1]) *
 				blendFactor;
+		}
+
+		public const float horizontalTerraceStepSize = 1f / terraceSteps;
+		public const float verticalTerraceStepSize = 1f / (terracesPerSlope + 1);
+
+		public static Vector3 TerraceLerp(Vector3 a, Vector3 b, int step) {
+			float h = step * HexMetrics.horizontalTerraceStepSize;
+			a.x += (b.x - a.x) * h;
+			a.z += (b.z - a.z) * h;
+			float v = ((step + 1) / 2) * HexMetrics.verticalTerraceStepSize;
+			a.y += (b.y - a.y) * v;
+			return a;
+		}
+
+		public static Color TerraceLerp(Color a, Color b, int step) {
+			float h = step * HexMetrics.horizontalTerraceStepSize;
+			return Color.Lerp(a, b, h);
 		}
 	}
 }
