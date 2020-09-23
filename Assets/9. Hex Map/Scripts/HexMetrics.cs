b3a948eb22207c3a64f0d8486ab0e21c621eb594
yeonhong park <yeonhong84@gmail.com>
2020-09-23 14:08:00 +0900

    [Blending Cell Colors] 3. Blend Regions - Triangulating Blend Regions - Edge Bridges - Filling the Gaps

--- Assets/9. Hex Map/Scripts/HexMetrics.cs
+++ Assets/9. Hex Map/Scripts/HexMetrics.cs
@@ -6,6 +6,8 @@ namespace HexMap
 	{
 		public const float outerRadius = 10f;
 		public const float innerRadius = outerRadius * 0.866025404f;
+		public const float solidFactor = 0.75f;
+		public const float blendFactor = 1f - solidFactor;
 
 		public static Vector3[] corners = {
 			new Vector3(0f, 0f, outerRadius),
@@ -23,6 +25,19 @@ namespace HexMap
 
 		public static Vector3 GetSecondCorner(HexDirection direction) {
 			return corners[(int)direction + 1];
+		}
+
+		public static Vector3 GetFirstSolidCorner(HexDirection direction) {
+			return corners[(int)direction] * solidFactor;
+		}
+
+		public static Vector3 GetSecondSolidCorner(HexDirection direction) {
+			return corners[(int)direction + 1] * solidFactor;
+		}
+
+		public static Vector3 GetBridge(HexDirection direction) {
+			return (corners[(int)direction] + corners[(int)direction + 1]) *
+				0.5f * blendFactor;
 		}
 	}
 }
