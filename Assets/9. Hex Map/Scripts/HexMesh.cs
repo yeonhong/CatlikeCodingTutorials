2f5bd5c37451cd37cf745af9eb4d1d69cfac57cb
yeonhong park <yeonhong84@gmail.com>
2020-10-07 13:31:39 +0900

    [Rivers] 5. Triangulating Adjacent to Rivers - Matching the Channel

--- Assets/9. Hex Map/Scripts/HexMesh.cs
+++ Assets/9. Hex Map/Scripts/HexMesh.cs
@@ -61,6 +61,9 @@ namespace HexMap
 						TriangulateWithRiver(direction, cell, center, e);
 					}
 				}
+				else {
+					TriangulateAdjacentToRiver(direction, cell, center, e);
+				}
 			}
 			else {
 				TriangulateEdgeFan(center, e, cell.Color);
@@ -71,6 +74,33 @@ namespace HexMap
 			if (direction <= HexDirection.SE) {
 				TriangulateConnection(direction, cell, e);
 			}
+		}
+
+		private void TriangulateAdjacentToRiver(
+			HexDirection direction, HexCell cell, Vector3 center, EdgeVertices e) {
+
+			if (cell.HasRiverThroughEdge(direction.Next())) {
+				if (cell.HasRiverThroughEdge(direction.Previous())) {
+					center += HexMetrics.GetSolidEdgeMiddle(direction) *
+						(HexMetrics.innerToOuter * 0.5f);
+				}
+				else if (cell.HasRiverThroughEdge(direction.Previous2())) {
+					center += HexMetrics.GetFirstSolidCorner(direction) * 0.25f;
+				}
+			}
+			else if (
+				cell.HasRiverThroughEdge(direction.Previous()) && 
+				cell.HasRiverThroughEdge(direction.Next2())) {
+				center += HexMetrics.GetSecondSolidCorner(direction) * 0.25f;
+			}
+
+			EdgeVertices m = new EdgeVertices(
+				Vector3.Lerp(center, e.v1, 0.5f),
+				Vector3.Lerp(center, e.v5, 0.5f)
+			);
+
+			TriangulateEdgeStrip(m, cell.Color, e, cell.Color);
+			TriangulateEdgeFan(center, m, cell.Color);
 		}
 
 		private void TriangulateWithRiverBeginOrEnd(
