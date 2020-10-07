316f77a00189d95cac47989378652fa2ea39b0f2
yeonhong park <yeonhong84@gmail.com>
2020-10-07 11:29:30 +0900

    [Rivers] 2. Editing Rivers - Detecting Drags - Adjusting Cells

--- Assets/9. Hex Map/Scripts/HexMapEditor.cs
+++ Assets/9. Hex Map/Scripts/HexMapEditor.cs
@@ -14,6 +14,17 @@ namespace HexMap
 		private bool applyElevation = true;
 		private int brushSize;
 
+		private enum OptionalToggle
+		{
+			Ignore, Yes, No
+		}
+
+		private OptionalToggle riverMode;
+
+		private bool isDrag;
+		private HexDirection dragDirection;
+		private HexCell previousCell;
+
 		private void Awake() {
 			SelectColor(0);
 		}
@@ -22,14 +33,42 @@ namespace HexMap
 			if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) {
 				HandleInput();
 			}
+			else {
+				previousCell = null;
+			}
 		}
 
 		private void HandleInput() {
 			Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit hit;
 			if (Physics.Raycast(inputRay, out hit)) {
-				EditCells(hexGrid.GetCell(hit.point));
+				HexCell currentCell = hexGrid.GetCell(hit.point);
+				if (previousCell && previousCell != currentCell) {
+					ValidateDrag(currentCell);
+				}
+				else {
+					isDrag = false;
+				}
+				EditCells(currentCell);
+				previousCell = currentCell;
 			}
+			else {
+				previousCell = null;
+			}
+		}
+
+		private void ValidateDrag(HexCell currentCell) {
+			for (
+				dragDirection = HexDirection.NE;
+				dragDirection <= HexDirection.NW;
+				dragDirection++
+			) {
+				if (previousCell.GetNeighbor(dragDirection) == currentCell) {
+					isDrag = true;
+					return;
+				}
+			}
+			isDrag = false;
 		}
 
 		private void EditCells(HexCell center) {
@@ -57,6 +96,16 @@ namespace HexMap
 				if (applyElevation) {
 					cell.Elevation = activeElevation;
 				}
+
+				if (riverMode == OptionalToggle.No) {
+					cell.RemoveRiver();
+				}
+				else if (isDrag && riverMode == OptionalToggle.Yes) {
+					HexCell otherCell = cell.GetNeighbor(dragDirection.Opposite());
+					if (otherCell) {
+						otherCell.SetOutgoingRiver(dragDirection);
+					}
+				}
 			}
 		}
 
@@ -81,6 +130,10 @@ namespace HexMap
 
 		public void ShowUI(bool visible) {
 			hexGrid.ShowUI(visible);
+		}
+
+		public void SetRiverMode(int mode) {
+			riverMode = (OptionalToggle)mode;
 		}
 	}
 }
